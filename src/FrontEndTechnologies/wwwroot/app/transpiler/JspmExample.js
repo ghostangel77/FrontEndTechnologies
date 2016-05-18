07dd9e1d7a7089e91eb8b4942aa3be25c2bb8a3b
Diego Torres <diego.torres@sterlingbackcheck.com>
2016-05-18 18:51:38 -0500

    9. Transpiler.

--- src/FrontEndTechnologies/wwwroot/app/transpiler/JspmExample.js
+++ src/FrontEndTechnologies/wwwroot/app/transpiler/JspmExample.js
@@ -1,4 +1,17 @@
-﻿var element = document.getElementById("viewContent");
-setTimeout(function () {
-    element.innerText = "Hello, JSPM!!!";
-}, 500);
+﻿import {Greeter} from './greeter';
+
+
+Greeter.greetStatic("staticContent", "Hello, from Static!!!");
+
+Greeter.greetStatic("defaultMsg");
+
+let greeter =  new Greeter();
+greeter.greet("viewContent", "Hello, from Instance!!!");
+
+
+
+
+//var element = document.getElementById("viewContent");
+//setTimeout(function () {
+//    element.innerText = "Hello, JSPM!!!";
+//}, 500);
---
+++ src/FrontEndTechnologies/wwwroot/app/transpiler/greeter.js
@@ -0,0 +1,11 @@
+﻿export class Greeter{
+    greet(id, msg){
+        let element = document.getElementById(id);
+        element.innerText = msg;
+    }
+
+    static greetStatic(id, msg="Default Message"){
+        let element = document.getElementById(id);
+        element.innerText = msg;
+    }
+}
