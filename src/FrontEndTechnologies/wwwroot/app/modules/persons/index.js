2d0c8864ee7fbdad69b5793466740073c217eeb2
Diego Torres <diego.torres@sterlingbackcheck.com>
2016-06-06 16:37:50 -0500

    15. Aurelia Example - Grid

---
+++ src/FrontEndTechnologies/wwwroot/app/modules/persons/index.js
@@ -0,0 +1,9 @@
+﻿
+export class Index{
+    constructor(){
+        this.persons=[
+            {id:1, firstName:"Diego", lastName:"Torres", phone:"234-234-234"},
+            {id:2, firstName:"Juan", lastName:"Perez", phone:"123-456-789"}
+        ];
+    }
+}
